0943bdc88cc515d6e6b95711a949525923e23a5f
Luca-Mariselli <lucamariselli@gmail.com>
2024-09-05 09:35:32 +0200

    Homepage cambiata

--- src/pages/AppHompage.vue
+++ src/pages/AppHompage.vue
@@ -16,16 +16,16 @@ export default {
     data() {
         return {
             // store,
-           
+
 
 
 
         }
     },
     methods: {
-       
 
-		
+
+
     },
 
     mounted() {
@@ -38,8 +38,51 @@ export default {
 </script>
 
 <template>
-    <h5 class="text-center">Hompage</h5>
-    <!-- <input type="text"> -->
+    <div class="jumbotron">
+        <div class="img-container">
+            <div class="d-flex flex-column justify-content-end align-items-center h-100">
+                <h1 class="display-5 fw-bold text-light">
+                    Welcome to BoolBnB
+                </h1>
+                <div class="searchbar-container d-flex align-items-center">
+                    <form class="d-flex col-6" role="search">
+                        <input class="searchbar me-3" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-success" type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.jumbo-img {
+    width: 100%
+}
+
+.img-container {
+    height: 40rem;
+    width: 100%;
+    background-size: 100%;
+    background-position-y: center;
+    background-repeat: no-repeat;
+    /* background-image: url({{asset('/img/' . 'colosseo.jpg')}}); */
+}
+
+.searchbar-container {
+    background-color: rgba(134, 134, 134, 0.815);
+    width: 80%;
+    border-radius: 80px;
+    margin-top: 2rem;
+    margin-bottom: 5rem;
+    padding: 1rem;
+}
+
+.searchbar {
+    border-radius: 80px;
+    height: 3rem;
+    background-color: rgba(240, 248, 255, 0);
+    border-color: white;
+
+}
+</style>
