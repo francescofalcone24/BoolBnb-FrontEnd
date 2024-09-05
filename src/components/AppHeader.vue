a49d76f359f825a3326824d7d36f6fcc0f4ef557
Davide Di Giacomo <davidedigiacomo001@gmail.com>
2024-09-05 09:36:31 +0200

    aggiunta rotta suites per prova

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,7 @@
 <script>
 import store from '../data/store';
 import axios from "axios";
+import { RouterLink, RouterView } from 'vue-router';
 
 export default {
 
@@ -35,31 +36,30 @@ export default {
 
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="/">Navbar</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <RouterLink to="/" class="nav-link">Home</RouterLink>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+          <!-- <a class="nav-link" href="#">Link</a> -->
+          <RouterLink to="/suites" class="nav-link">Suites</RouterLink>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
+            Dev Team
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li><a class="dropdown-item" href="#">Antonio Dattolo</a></li>
+            <li><a class="dropdown-item" href="#">Francesco Falcone</a></li>
+            <!-- <li><hr class="dropdown-divider"></li> -->
+            <li><a class="dropdown-item" href="#">Luca Mariselli</a></li>
+            <li><a class="dropdown-item" href="#">Davide Di Giacomo</a></li>
           </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
         </li>
       </ul>
       <form class="d-flex" role="search">
