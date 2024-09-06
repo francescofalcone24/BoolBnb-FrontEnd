d3a1b1d5e02f9c022f51d07545a93b9a2ba7342d
AntonioDattolo <antonio19794.a@gmail.com>
2024-09-06 11:00:58 +0200

    first css for ANTONIO

--- src/pages/AppSingleSuite.vue
+++ src/pages/AppSingleSuite.vue
@@ -73,20 +73,101 @@ export default {
 
     </div> -->
     <div class="container d-flex py-2 justify-content-between col-12" v-if="store.singleSuite">
-        
+
         <img v-if="!store.singleSuite.img.startsWith('http')" style="height: 50rem;"
-        :src="store.localHostUrl + '/storage/' + store.singleSuite.img" class=" w-50" alt="...">
-        
+            :src="store.localHostUrl + '/storage/' + store.singleSuite.img" class=" w-50" alt="...">
+
         <img v-else="" :src="store.singleSuite.img" class=" w-50" alt="..." style="height: 50rem;">
         <div class="d-flex flex-column justify-content-center">
             <h5 class="card-title">{{ store.singleSuite.title }}</h5>
             <p class="card-text">{{ store.singleSuite.address }}</p>
             <router-link :to="{ name: 'suites' }" class="btn btn-outline-danger">back to the list</router-link>
 
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">
+                    <div class="col-6 d-flex justify-content-between">
+
+                        <i class="fa-solid fa-person-shelter col-6"></i>
+
+
+                        <strong class="col-6">
+                            Rooms :
+                        </strong>
+                    </div>
+                    <span>
+
+                        {{ store.singleSuite.room }}
+                    </span>
+                </li>
+
+                <li class="list-group-item">
+                    <div class="col-6 d-flex justify-content-between">
+
+                        <i class="fa-solid fa-toilet col-6"></i>
+
+
+                        <strong class="col-6">
+                            Bathrooms :
+                        </strong>
+                    </div>
+                    <span>
+
+                        {{ store.singleSuite.bathroom }}
+                    </span>
+                </li>
+
+                <li class="list-group-item">
+                    <div class="col-6 d-flex justify-content-between">
+
+                        <i class="fa-solid fa-bed col-6"></i>
+
+
+                        <strong class="col-6">
+                            Beds :
+                        </strong>
+                    </div>
+                    <span>
+
+                        {{ store.singleSuite.bed }}
+                    </span>
+                </li>
+
+                <li class="list-group-item">
+                    <div class="col-6 d-flex justify-content-between">
+
+                        <i class="fa-solid fa-maximize col-6"></i>
+
+
+                        <strong class="col-6">
+                            Square-Meters :
+                        </strong>
+                    </div>
+                    <span>
+
+                        {{ store.singleSuite.squareM }}
+                    </span>
+                </li>
+
+            </ul>
+
         </div>
     </div>
 </template>
 
 <style scoped>
+li {
+    font-size: 30px;
+    display: flex;
+    justify-content: space-between;
+}
 
+strong {
+    font-size: 15px;
+    white-space: nowrap;
+    margin-right: 2rem;
+}
+
+i {
+    width: 10px;
+}
 </style>
