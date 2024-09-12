c44e63bb5fe1019fa42486d352b23b8d82c5bffe
AntonioDattolo <antonio19794.a@gmail.com>
2024-09-12 19:35:12 +0200

    frontend

--- src/pages/AppSuites.vue
+++ src/pages/AppSuites.vue
@@ -34,7 +34,9 @@ export default {
             myApi: null,
             check: [],
             services: [],
-            service_url: ''
+            service_url: '',
+            suite_art : 'd-none',
+            loading_art : '',
 
         }
     },
@@ -203,7 +205,11 @@ export default {
                     console.log(error);
                 });
                 // console.log(this.filtered[0].distance , 'al mounted')
-                
+             
+                setTimeout(() => {
+                    this.suite_art = ''
+                    this.loading_art = 'd-none'
+                }, 2376);
         },
 
     },
@@ -256,7 +262,12 @@ export default {
 
             <!-- FILTERS BAR -->
             <div class="my-left-bar col-2 p-0 border border-dark my-2 h-100 sticky-top z-1 bg-warning-subtle">
-                <h4 class="border-bottom border-dark p-3 text-center m-0">Results: {{ filtered.length }}</h4>
+                <h4 class="border-bottom border-dark p-3 text-center m-0">
+                    Results:
+                    <span :class="suite_art">
+                        {{ filtered.length }}
+                    </span>
+                </h4>
 
                 <h5 class="border-bottom border-dark p-1 my-3">Filter by:</h5>
                 <div class="filtri-brack">
@@ -291,9 +302,18 @@ export default {
 
             <!-- ***************************************SUITE CARDS****************************************************** -->
 
+            <div :class="loading_art" class="load-wrapp container-fluid justify-self-center">
+                <div class="load-9" style=" position:absolute; left :48%;top : 25%;">
+
+                    <div class="spinner" style="width : 175px">
+                        <div class="bubble-1"></div>
+                        <div class="bubble-2"></div>
+                    </div>
+                </div>
+            </div>
 
 
-            <div class="col-xl-8 col-lg-9 col-md-9 col-md-12">
+            <div :class="suite_art" class="col-xl-8 col-lg-9 col-md-9 col-md-12">
                 <!-- CARD SPONSORIZZATE -->
                 <div v-for="suite in filtered">
                     <router-link :to="{ name: 'AppSingleSuite', params: { slug: suite.slug } }"
@@ -336,20 +356,18 @@ export default {
                                     </div>
                                 </div>
                             </div>
+
                             <!-- SERVICES QUI QUANDO LI ABBIAMO -->
-                            <div class="ms-4 mt-2 col-2">
-
-
-
-
-                                <div>
-                                    <strong>Services:</strong>
-                                    <div v-for="service in suite.services">
-                                        <br>
-                                        {{ service.name }}
-
+                            <div class="p-3 col-3 d-flex justify-content-start flex-wrap">
+                                    <span class="col-12" style="font-size: 25px;">
+                                    Services:
+                                    </span>
+                                    <div class="d-flex col-3 flex-row flex-wrap"
+                                        v-for="service in suite.services">
+                                        <div class="d-flex gap">
+                                                <i :class="service.icon" class="col-3" style=""></i>
+                                        </div>
                                     </div>
-                                </div>
                             </div>
                         </div>
                     </router-link>
@@ -357,7 +375,7 @@ export default {
 
 
                 <!-- CARD NON SPONSORIZZATE -->
-                <div v-for="suite in filtered">
+                <div v-for="suite in filtered" :class="suite_art">
                     <router-link
                         :to="{ name: 'AppSingleSuite', params: { slug: suite.slug }, query: { slug: suite.slug } }"
                         class="text-decoration-none text-dark">
@@ -490,6 +508,300 @@ export default {
 #result {
     z-index: 99;
 }
+/*  */
+.content {
+  padding: 15px;
+  overflow: hidden;
+  background-color: #e7e7e7;
+  background-color: rgba(0, 0, 0, 0.06);
+}
+
+h1 {
+  padding-bottom: 15px;
+  border-bottom: 1px solid #d8d8d8;
+  font-weight: 600;
+}
+
+h1 span {
+  font-family: monospace, serif;
+}
+
+h3 {
+  padding-bottom: 20px;
+  /* box-shadow: 0 1px 0 rgba(0, 0, 0, 0.1), 0 2px 0 rgba(255, 255, 255, 0.9); */
+}
+
+p {
+  margin: 0;
+  padding: 10px 0;
+  color: #777;
+}
+
+.clear {
+  clear: both;
+}
+
+/* -----------------------------------------
+  =CSS3 Loading animations
+-------------------------------------------- */
+
+/* =Elements style
+---------------------- */
+/* .load-wrapp {
+  justif
+  width: 300px;
+  height: 100px;
+  margin: 0 10px 10px 0;
+  padding: 20px 20px 20px;
+  border-radius: 5px;
+  text-align: center;
+  background-color: #d8d8d8;
+} */
+
+.load-wrapp p {
+  padding: 0 0 20px;
+}
+.load-wrapp:last-child {
+  margin-right: 0;
+}
+
+.line {
+  display: inline-block;
+  width: 15px;
+  height: 15px;
+  border-radius: 15px;
+  background-color: #4b9cdb;
+}
+
+.ring-1 {
+  width: 10px;
+  height: 10px;
+  margin: 0 auto;
+  padding: 10px;
+  border: 7px dashed #4b9cdb;
+  border-radius: 100%;
+}
+
+.ring-2 {
+  position: relative;
+  width: 45px;
+  height: 45px;
+  margin: 0 auto;
+  border: 4px solid #4b9cdb;
+  border-radius: 100%;
+}
+
+.ball-holder {
+  position: absolute;
+  width: 12px;
+  height: 45px;
+  left: 17px;
+  top: 0px;
+}
+
+.ball {
+  position: absolute;
+  top: -11px;
+  left: 0;
+  width: 16px;
+  height: 16px;
+  border-radius: 100%;
+  background: #4282b3;
+}
+
+.letter-holder {
+  padding: 16px;
+}
+
+.letter {
+  float: left;
+  font-size: 14px;
+  color: #777;
+}
+
+.square {
+  width: 12px;
+  height: 12px;
+  border-radius: 4px;
+  background-color: #4b9cdb;
+}
+
+.spinner {
+  position: relative;
+  width: 45px;
+  height: 45px;
+  margin: 0 auto;
+}
+
+.bubble-1,
+.bubble-2 {
+  position: absolute;
+  top: 0;
+  width: 55px;
+  height: 55px;
+  border-radius: 100%;
+  background-color: #4b9cdb;
+}
+
+.bubble-2 {
+  top: auto;
+  bottom: 0;
+}
+
+.bar {
+  float: left;
+  width: 15px;
+  height: 6px;
+  border-radius: 2px;
+  background-color: #4b9cdb;
+}
+
+/* =Animate the stuff
+------------------------ */
+
+
+
+
+
+
+.l-9 {
+  animation-delay: 1.44s;
+}
+
+
+.load-9 .spinner {
+  animation: loadingI 2s linear infinite;
+}
+.load-9 .bubble-1,
+.load-9 .bubble-2 {
+  animation: bounce 2s ease-in-out infinite;
+}
+.load-9 .bubble-2 {
+  animation-delay: -1s;
+}
+
+
+
+@keyframes loadingA {
+  0% {
+    height: 15px;
+  }
+  50% {
+    height: 75px;
+  }
+  100% {
+    height: 15px;
+  }
+}
+
+@keyframes loadingB {
+  0% {
+    width: 15px;
+  }
+  50% {
+    width: 75px;
+  }
+  100% {
+    width: 15px;
+  }
+}
+
+@keyframes loadingC {
+  0% {
+    transform: translate(0, 0);
+  }
+  50% {
+    transform: translate(0, 45px);
+  }
+  100% {
+    transform: translate(0, 0);
+  }
+}
+
+@keyframes loadingD {
+  0% {
+    transform: rotate(0deg);
+  }
+  50% {
+    transform: rotate(180deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+@keyframes loadingE {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+@keyframes loadingF {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+
+@keyframes loadingG {
+  0% {
+    transform: translate(0, 0) rotate(0deg);
+  }
+  50% {
+    transform: translate(70px, 0) rotate(360deg);
+  }
+  100% {
+    transform: translate(0, 0) rotate(0deg);
+  }
+}
+
+@keyframes loadingH {
+  0% {
+    width: 15px;
+  }
+  50% {
+    width: 75px;
+    padding: 4px;
+  }
+  100% {
+    width: 15px;
+  }
+}
+
+@keyframes loadingI {
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+@keyframes bounce {
+  0%,
+  100% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1);
+  }
+}
+
+@keyframes loadingJ {
+  0%,
+  100% {
+    transform: translate(0, 0);
+  }
+
+  50% {
+    transform: translate(80px, 0);
+    background-color: #f5634a;
+    width: 25px;
+  }
+}
+
 
 @media only screen and (max-width: 992px) {
     .my-left-bar {
@@ -513,4 +825,7 @@ export default {
         align-self: center;
     }
 }
+
+
+
 </style>
